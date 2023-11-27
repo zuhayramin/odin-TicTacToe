a7138a5cac669ddcbd358da4b0b53d7516a80ae3
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-11-27 11:10:55 +0400

    Add function to alternate turns and update board after moves

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-function makePlayer(playerName) {
-    handle = playerName
-    return function player(playerSign) {
-        sign = playerSign
-        return { handle, sign }
+function makeGameboard() {
+    const gameBoard = {
+        grid: [
+            ["", "", ""], // first row
+            ["", "", ""], // second row
+            ["", "", ""], // third row
+        ],
+        // Prints gameboard in console
+        // Will be updated to display board in DOM
+        display: function () {
+            for (let row = 0; row < 3; row++) {
+                // loops over each row
+                console.log(
+                    ` ${this.grid[row][0]} | ${this.grid[row][1]} | ${this.grid[row][2]} `
+                )
+                // prints a separator line
+                if (row < 2) {
+                    console.log("---------")
+                }
+            }
+        },
     }
+
+    return gameBoard
+}
+
+const makePlayer = (name, sign) => {
+    return { name, sign }
 }
 
 function checkWinningCombos(grid) {
@@ -48,51 +70,60 @@ function checkWinningCombos(grid) {
     return false
 }
 
+// Function to handle player moves
+function handlePlayerMove(board, player) {
+    // Function to validate user input
+    function isValidMove(row, col) {
+        return (
+            row >= 0 &&
+            row < 3 &&
+            col >= 0 &&
+            col < 3 &&
+            board.grid[row][col] === ""
+        )
+    }
+
+    // Function to get valid user input
+    function getUserMove() {
+        let row, col
+        do {
+            row = parseInt(
+                prompt(`Enter the row (0, 1, or 2) for ${player.name}:`)
+            )
+            col = parseInt(
+                prompt(`Enter the column (0, 1, or 2) for ${player.name}:`)
+            )
+        } while (!isValidMove(row, col))
+
+        return { row, col }
+    }
+
+    // Get valid user input
+    const { row, col } = getUserMove()
+
+    // Update the board with the player's move
+    board.grid[row][col] = player.sign
+}
+
 const gameController = function () {
     // Spaces to improve readability in console
     console.log("\nWelcome to Tic Tac Toe\n")
 
     // Create gameboard
-    const gameBoard = {
-        grid: [
-            ["X", "X", "X"], // first row
-            ["", "", ""], // second row
-            ["", "", ""], // third row
-        ],
-        // Prints gameboard in console
-        // Will be updated to display board in DOM
-        display: function () {
-            for (let row = 0; row < 3; row++) {
-                // loops over each row
-                console.log(
-                    ` ${this.grid[row][0]} | ${this.grid[row][1]} | ${this.grid[row][2]} `
-                )
-                // prints a separator line
-                if (row < 2) {
-                    console.log("---------")
-                }
-            }
-        },
-    }
+    let board = makeGameboard()
 
     // Create Player 1
-    let nameOfPlayer1 = makePlayer("Player 1")
-    let player1 = nameOfPlayer1("X")
+    let player1 = makePlayer("Player 1", "X")
 
     // Create Player 2
-    let nameOfPlayer2 = makePlayer("Player 2")
-    let player2 = nameOfPlayer2("O")
+    let player2 = makePlayer("Player 2", "O")
 
     let activePlayer = player1
 
-    if (checkWinningCombos(gameBoard.grid)) {
-        console.log("Gameover")
-    }
-
-    gameBoard.display()
-    // console.log(
-    //     "\nIt is Player 1's move! Choose a box by indicating the column and row number: "
-    // )
+    do {
+        console.log(activePlayer.name)
+        handlePlayerMove(board, activePlayer)
+        board.display()
+        activePlayer = activePlayer === player1 ? player2 : player1
+    } while (!checkWinningCombos(board.grid))
 }
-
-gameController()
